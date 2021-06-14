dcf4ea8d17840b38e80a3603c65cb5f570512556
Nyxordinal <nyxordinal@gmail.com>
2021-06-15 01:45:12 +0700

    First commit

---
+++ src/components/Layout.tsx
@@ -0,0 +1,19 @@
+import styles from '@style/Home.module.css';
+import { ReactElement } from 'react';
+import Footer from './Footer';
+import Header from './Header';
+
+const Layout = (props: { children: ReactElement }) => {
+  const { children } = props;
+  return (
+    <div className={styles.container}>
+      <Header />
+
+      {children}
+
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;
---
+++ src/services/api.ts
@@ -0,0 +1,18 @@
+import axios, { AxiosError, AxiosInstance, AxiosResponse } from 'axios';
+
+const API_URL = process.env.NEXT_PUBLIC_API_URL;
+
+export const API: AxiosInstance = axios.create({
+  baseURL: API_URL,
+  headers: { Accept: 'application/json' },
+});
+
+const handleResponseReceive = (response: AxiosResponse) => {
+  return response.data;
+};
+
+const handleResponseError = (error: AxiosError) => {
+  return error.response ? error.response.data : error;
+};
+
+API.interceptors.response.use(handleResponseReceive, handleResponseError);
